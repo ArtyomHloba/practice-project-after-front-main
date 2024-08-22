ad15b7f7c9757921742fb1c07197171bd2b15ffc
ArtyomHloba <hlobartyom@gmail.com>
2024-08-22 18:06:49 +0300

    Add styles for FirstNamingPlatform and fix padding in NamingAgency

--- client/src/pages/Home/FirstNamingPlatform/FirstNamingPlatform.jsx
+++ client/src/pages/Home/FirstNamingPlatform/FirstNamingPlatform.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import styles from './FirstNamingPlatform.module.sass';
 import data from './data';
 import CONSTANTS from './../../../constants';
 
@@ -6,21 +7,31 @@ const { STATIC_IMAGES_PATH } = CONSTANTS;
 
 function FirstNamingPlatform () {
   const mapItems = ({ icon, title, body }, i) => (
-    <li key={i}>
-      <img src={icon} alt='icon' />
-      <h3>{title}</h3>
-      <p>{body}</p>
+    <li key={i} className={styles.featureItem}>
+      <img src={icon} alt='icon' className={styles.featureIcon} />
+      <h3 className={styles.featureTitle}>{title}</h3>
+      <p className={styles.featureBody}>{body}</p>
     </li>
   );
 
   return (
-    <section>
-      <h2>Atom: The World's #1 Business Naming Platform</h2>
-      <ul>{data.map(mapItems)}</ul>
-      <div>
-        <img src={`${STATIC_IMAGES_PATH}home/nh-theworld5.svg`} alt='' />
-        <p>The Ultra Premium Marketplace Is Here</p>
-        <Link type='#'>Explore now</Link>
+    <section className={styles.sectionContainer}>
+      <h2 className={styles.sectionTitle}>
+        Atom: The World's #1 Business Naming Platform
+      </h2>
+      <ul className={styles.featuresList}>{data.map(mapItems)}</ul>
+      <div className={styles.premiumMarketplace}>
+        <img
+          src={`${STATIC_IMAGES_PATH}home/nh-theworld5.svg`}
+          alt=''
+          className={styles.marketplaceIcon}
+        />
+        <p className={styles.marketplaceText}>
+          The Ultra Premium Marketplace Is Here
+        </p>
+        <Link to='#' className={styles.exploreButton}>
+          Explore now
+        </Link>
       </div>
     </section>
   );
