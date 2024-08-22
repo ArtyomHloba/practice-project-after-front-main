a3ea3cec6b813e1e33e695931c10b51517c2821c
ArtyomHloba <hlobartyom@gmail.com>
2024-08-22 15:26:00 +0300

    Add FirstNamingPlatform component

---
+++ client/src/pages/Home/FirstNamingPlatform/FirstNamingPlatform.jsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom';
+import data from './data.json';
+
+console.log(data);
+function FirstNamingPlatform () {
+  const mapItems = ({ icon, title, body }, i) => (
+    <li key={i}>
+      <img src={icon} alt='icon' />
+      <h3>{title}</h3>
+      <p>{body}</p>
+    </li>
+  );
+
+  return (
+    <section>
+      <h2>Atom: The World's #1 Business Naming Platform</h2>
+      <ul>{data.map(mapItems)}</ul>
+      <div>
+        <img src='/staticImages/home/nh-theworld5.svg' alt='' />
+        <p>The Ultra Premium Marketplace Is Here</p>
+        <Link type='#'>Explore now</Link>
+      </div>
+    </section>
+  );
+}
+
+export default FirstNamingPlatform;
